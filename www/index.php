4a72364b4ba088e7feca910ac9602297d904aae4
guilleatm <47484930+guilleatm@users.noreply.github.com>
2025-01-29 19:19:29 +0100

    Millores generals i php simplificat (#3)
    * m'he cansat de posar mes coses
    
    * remove test td
    
    * millores i simplicitat

--- www/index.php
+++ www/index.php
@@ -19,18 +19,13 @@
 
     <body>
 
-        <!-- DATABASE CONFIG -->
         <?php
-            $servername = "mysql";
-            $username = "asirweb";
-            $password = "qwe_123";
-            $dbname = "webasir";
+            include 'database_connect.php';
+            
+            $sql = "SELECT * FROM cars";
+            $result = $conn->query($sql);
 
-            $conn = new mysqli($servername, $username, $password, $dbname);
-            if ($conn->connect_error)
-            {
-                die("Conexión fallida: " . $conn->connect_error);
-            }
+            include 'database_dispose.php';
         ?>
     
         <header>
@@ -85,7 +80,7 @@
 
 
             <div class="container">
-                <h1>Coches disponibles</h1>
+                <h1>Coches disponibles <i class="bi bi-truck"></i></h1>
                 
                 <hr>
 
@@ -105,32 +100,30 @@
                         </thead>
                         <tbody>
 
-                            <?php
-                                $sql = "SELECT * FROM cars";
-                                $result = $conn->query($sql);
-                                
-								if ($result->num_rows > 0)
-								{
-									while ( $row = $result->fetch_assoc() )
-									{
-										echo "  <tr>
-													<td>#{$row['license_plate']}</td>
-													<td>{$row['brand']}</td>
-													<td>{$row['model']}</td>
-													<td>
-														<div class='btn-group'>
-															<a href='car_detail.php?license_plate={$row['license_plate']}&brand={$row['brand']}&model={$row['model']}' class='btn btn-outline-success btn-md'><i class='bi bi-eye'></i> Detalles</a>
-															<a href='#' class='btn btn-outline-primary btn-md'><i class='bi bi-pen'></i> Editar</a>
-														</div>
-													</td>
-												</tr>";
-									}            
-								}
-								else
-								{
-									echo "<tr><td class='danger' colspan='5'>No hay datos disponibles</td></tr>";
-								}
-                            ?>
+                        <?php
+    
+                            if ($result->num_rows > 0)
+                            {
+                                while ( $row = $result->fetch_assoc() )
+                                {
+                                    echo "  <tr>
+                                                <td>{$row['license_plate']}</td>
+                                                <td>{$row['brand']}</td>
+                                                <td>{$row['model']}</td>
+                                                <td>
+                                                    <div class='btn-group'>
+                                                        <a href='car_detail.php?license_plate={$row['license_plate']}' class='btn btn-outline-success btn-md'><i class='bi bi-eye'></i> Detalles</a>
+                                                        <button type='button' class='btn btn-outline-primary' data-toggle='modal' data-target='#staticBackdrop'><i class='bi bi-pen'></i> Editar</button>
+                                                    </div>
+                                                </td>
+                                            </tr>";
+                                }            
+                            }
+                            else
+                            {
+                                echo "<tr><td class='danger' colspan='5'>No hay datos disponibles</td></tr>";
+                            }
+                        ?>
                         </tbody>
                     </table>
                 </div>
@@ -138,6 +131,30 @@
             </div>
 
         </main>
+
+        <!-- MODALS -->
+		<div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+			<div class="modal-dialog">
+				<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="staticBackdropLabel">
+						<?php
+							echo "Eres un fraude!";
+						?>
+					</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					Vaya, parece que no tienes permisos de administración, a quien quieres engañar?
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-danger" data-dismiss="modal">Oops</button>
+				</div>
+				</div>
+			</div>
+		</div>
 
         <footer>
 
@@ -153,11 +170,6 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-        <!-- DISPOSE CONNECTION -->
-        <?php
-            $conn = null;
-        ?>
 
     </body>
 </html>
